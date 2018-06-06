c33735805a770fceaef754a8c96e5359faeb429c
seiya_kumada <kumada@kumadakiyoshinarinoMacBook-Pro.local>
2018-06-06 22:38:14 +0900

    I have implemented an encoder.

---
+++ README.md
@@ -0,0 +1,6 @@
+# net.py
+## Encoder
+
+Encoder takes 'x' and 'eps' as inputs.
+After 'eps' is transformed by the linear transformation, it is added to x.
+An altenative method to merge 'x' and 'eps' is to concatenate them.
--- net.py
+++ net.py
@@ -3,6 +3,10 @@
 import xavier
 import chainer
 import chainer.links as L
+import chainer.functions as F
+
+# what does 'reuse' in tensorflow mean?
+# https://qiita.com/halhorn/items/6805b1fd3f8ff74840df
 
 
 class Encoder(chainer.Chain):
@@ -10,13 +14,54 @@ class Encoder(chainer.Chain):
     def __init__(self, x_dim, eps_dim, h_dim=512):
         super(Encoder, self).__init__()
         with self.init_scope():
-            self.l1 = L.Linear(x_dim, eps_dim, initialW=xavier.Xavier(x_dim, eps_dim))
-            self.l2 = L.Linear(eps_dim, h_dim, initialW=xavier.Xavier(eps_dim, h_dim))
-            self.l3 = L.Linear(h_dim, eps_dim, initialW=xavier.Xavier(h_dim, eps_dim))
+            self.a1 = L.Linear(eps_dim, x_dim, initialW=xavier.Xavier(eps_dim, x_dim))
+            self.a2 = L.Linear(eps_dim, h_dim, initialW=xavier.Xavier(eps_dim, h_dim))
+            self.a3 = L.Linear(eps_dim, h_dim, initialW=xavier.Xavier(eps_dim, h_dim))
+
+            self.l1 = L.Linear(x_dim, h_dim, initialW=xavier.Xavier(eps_dim, h_dim))
+            self.l2 = L.Linear(h_dim, h_dim, initialW=xavier.Xavier(h_dim, h_dim))
+            self.l3 = L.Linear(h_dim, eps_dim, initialW=chainer.initializers.Normal(scale=1e-5))
+
+    def merge_1(self, x, eps):
+        h = self.a1(eps)
+        return h + x
+
+    def merge_2(self, x, eps):
+        h = self.a2(eps)
+        return h + x
+
+    def merge_3(self, x, eps):
+        h = self.a3(eps)
+        return h + x
 
     def __call__(self, x, eps):
-        w = self.l1(x)
-        h = w + eps
+        h = self.merge_1(x, eps)
+        h = self.l1(h)
+        h = F.softplus(h)
+
+        h = self.merge_2(h, eps)
         h = self.l2(h)
+        h = F.softplus(h)
+
+        h = self.merge_3(h, eps)
         h = self.l3(h)
         return h
+
+
+if __name__ == '__main__':
+    import unittest
+    import numpy as np
+
+    class TestEncoder(unittest.TestCase):
+
+        def test_call(self):
+            batch_size = 3
+            x_dim = 4
+            eps_dim = 2
+            x = chainer.Variable(np.arange(batch_size * x_dim).reshape(batch_size, x_dim).astype(np.float32))
+            eps = chainer.Variable(np.arange(batch_size * eps_dim).reshape(batch_size, eps_dim).astype(np.float32))
+            encoder = Encoder(x_dim, eps_dim)
+            z = encoder(x, eps)
+            self.assertTrue(z.shape == (batch_size, eps_dim))
+
+    unittest.main()
